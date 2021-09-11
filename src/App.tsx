977589b234b2c9ec72f2748909b91420c16caf2b
JavierPoduje <javier.poduje@gmail.com>
2021-09-11 13:26:06 -0300

    Setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './styles.css';
 
-import { ClickCounter } from './ClickCounter';
-
 export const App = () => {
   return (
     <>
       <h1>Hola Sort Visualizer - {process.env.name}</h1>
-      <ClickCounter />
     </>
   );
 };
